8c216b804ebb479daff7edd06e2b622dd8e3ffec
fly886 <fly886@fly886.com>
2020-03-03 20:52:06 +0800

    fix

--- JS/rrtv.js
+++ JS/rrtv.js
@@ -1,3 +1,17 @@
+/*
+人人视频解锁原画
+
+[task_local]
+1 0 * * * hdcj.js
+
+[rewrite_local]
+#人人视频（加上了看电影的解锁）
+^https:\/\/api\.rr\.tv(\/user\/privilege\/list|\/ad\/getAll|\/rrtv-video\/v4plus\/season\/detail) url script-response-body QuanJS/JS/rrtv.js
+
+[mitm]
+hostname = api.rr.tv
+*/
+
 var body = $response.body;
 var url = $request.url;
 
